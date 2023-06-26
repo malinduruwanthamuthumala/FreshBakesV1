fdc0b5b90cb827e330b34aa01f9d80a565f90b84
malinduruwantha <malinduruwantha@gmail.com>
2023-06-26 11:28:31 +0530

    add mealAmountController

--- src/components/Layouts/Card.js
+++ src/components/Layouts/Card.js
@@ -1,55 +1,96 @@
 import React from "react";
 import {
-    MDBCard,
-    MDBCardImage,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCardLink,
-    MDBListGroup,
-    MDBListGroupItem,
-    MDBIcon,
-  } from 'mdb-react-ui-kit';
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardLink,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBIcon,
+} from "mdb-react-ui-kit";
 import "./Card.css";
+import MealAmountController from "../UI/MealAmountController";
 
-const Card= (props)=>{
-    return(
-        <MDBCard>
-      <MDBCardImage position='top' alt='...' src={props.src} className="image"/>
+const Card = (props) => {
+  const MEDIUM = "medium";
+  const LARGE = "large";
+  const updateMealAmounts = (type, counter) => {
+    switch (type) {
+      case MEDIUM:
+        setMediumAmount(counter);
+        break;
+      case LARGE:
+        setLargeAmount(counter);
+    }
+  };
+
+  let [mediumAmount, setMediumAmount] = React.useState(0);
+  let [largeAmount, setLargeAmount] = React.useState(0);
+  return (
+    <MDBCard>
+      <MDBCardImage
+        position="top"
+        alt="..."
+        src={props.src}
+        className="image"
+      />
       <MDBCardBody>
         <MDBCardTitle className="card_title title">{props.name}</MDBCardTitle>
         <MDBCardText className="left description">
           {props.description}
         </MDBCardText>
-        <p className="left ingredients" >{props.incredients.map(meal => {
-            return ( meal + ",")
-        })}</p>
+        <p className="left ingredients">
+          {props.incredients.map((meal) => {
+            return meal + ",";
+          })}
+        </p>
       </MDBCardBody>
       <div className="row">
-        <div className="col-md-6 right price_label">
-            <p>Medium</p>
+        <div className="col-md-4 right price_label">
+          <p>Medium</p>
         </div>
-        <div className="col-md-6 price_value">
-            <p>$ {props.prices[0]}</p>
+        <div className="col-md-4 price_value">
+          <p>$ {props.prices[0]}</p>
         </div>
-        
-       </div>
-       <div className="row">
-        <div className="col-md-6 right price_label">
-            <p>Large</p>
+        <div className="col-md-4 price_value">
+          <MealAmountController
+            updateParent={updateMealAmounts}
+            type={MEDIUM}
+            
+          ></MealAmountController>
         </div>
-        <div className="col-md-6 price_value">
-            <p> $ {props.prices[1]}</p>
+      </div>
+      <div className="row">
+        <div className="col-md-4 right price_label">
+          <p>Large</p>
         </div>
-        
-       </div>
+        <div className="col-md-4 price_value">
+          <p> $ {props.prices[1]}</p>
+        </div>
+        <div className="col-md-4 price_value">
+          <MealAmountController
+            updateParent={updateMealAmounts}
+            type={LARGE}
+          ></MealAmountController>
+        </div>
+      </div>
       <MDBCardBody>
-      <MDBIcon fas icon="shopping-cart" size="lg" className="left shopping_cart" />
-      
-       
+        <div className="row">
+          <div className="col-md-10 right">Add To Cart</div>
+          <div className="col-md-2 ">
+            <MDBIcon
+              fas
+              icon="shopping-cart"
+              size="lg"
+              className="left shopping_cart"
+            />
+          </div>
+        </div>
       </MDBCardBody>
     </MDBCard>
-    );
-}
+  );
+};
 
-export default Card
+export default Card;
